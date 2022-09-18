ae732b64233e990f68f80163d501fb8a6917a754
pviethung <pv2hung97@gmail.com>
2022-09-18 22:07:56 +0700

    add monaco-editor

---
+++ src/components/CodeEditor.tsx
@@ -0,0 +1,12 @@
+import Editor from '@monaco-editor/react';
+
+const CodeEditor = () => {
+  return (
+    <Editor
+      height="90vh"
+      defaultLanguage="javascript"
+      defaultValue="// some comment"
+    />
+  );
+};
+export default CodeEditor;
