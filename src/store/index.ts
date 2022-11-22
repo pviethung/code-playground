ddd12dfd0745d80ff1219b60039826241e89971c
pviethung <pv2hung97@gmail.com>
2022-11-22 17:45:30 +0700

    clear log

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,7 +6,4 @@ const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export type RootState = ReturnType<typeof store.getState>;
 
-store.subscribe(() => {
-  console.log(store.getState());
-});
 export { store };
